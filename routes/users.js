cdda5d23abeaea45b86c29508f5d8ac469ed733c
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-03-09 14:21:08 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+});
+
+// GET one user
+router.get('/:id', (req, res) => {
+});
+
+// GET all shows watched by a user (user id in req.params)
+router.get('/:id/shows', (req, res) => {
+});
+
+// PUT update and add a show if a user has watched it
+router.put('/:id/shows', (req, res) => {
+});
+
+module.exports = router;
 
