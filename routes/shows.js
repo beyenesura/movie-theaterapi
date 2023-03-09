2bf864ed2556f5930c7868d1b7ffa1c230b87043
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-03-09 14:21:45 -0500

    Update shows.js

--- routes/shows.js
+++ routes/shows.js
@@ -1,1 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+// GET all shows
+router.get('/', (req, res) => {
+});
+
+// GET one show
+router.get('/:id', (req, res) => {
+});
+
+// GET shows of a particular genre (genre in req.params)
+router.get('/genre/:genre', (req, res) => {
+});
+
+// PUT update rating of a show that has been watched
+router.put('/:id/rating', (req, res) => {
+});
+
+// PUT update the status of a show
+router.put('/:id/status', (req, res) => {
+});
+
+// DELETE a show
+router.delete('/:id', (req, res) => {
+});
+
+module.exports = router;
 
